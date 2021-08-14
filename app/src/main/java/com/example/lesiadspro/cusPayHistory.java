e167682aa1b68ee2b7b8fb41ad6ba5ceca0d30c2
Bihini Amarasinghe <bihiniamarasinghe98@gmail.com>
2021-08-14 21:29:51 +0530

    Customer side payment history

---
+++ app/src/main/java/com/example/lesiadspro/cusPayHistory.java
@@ -0,0 +1,14 @@
+package com.example.lesiadspro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cusPayHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cus_pay_history);
+    }
+}
