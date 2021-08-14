6956f26853aa7f1adaabbf7ed530ff41c03fa657
Bihini Amarasinghe <bihiniamarasinghe98@gmail.com>
2021-08-14 17:50:01 +0530

    Edit Payment details

---
+++ app/src/main/java/com/example/lesiadspro/Editpayment.java
@@ -0,0 +1,14 @@
+package com.example.lesiadspro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Editpayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editpayment);
+    }
+}
