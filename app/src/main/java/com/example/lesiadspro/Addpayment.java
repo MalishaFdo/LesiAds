8f38acad3016d90a7cc2d60113804eaf7b9a2cd1
Bihini Amarasinghe <bihiniamarasinghe98@gmail.com>
2021-08-14 13:21:41 +0530

    Initiate Payment

--- app/src/main/java/com/example/lesiadspro/Addpayment.java
+++ app/src/main/java/com/example/lesiadspro/Addpayment.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Editfeedback extends AppCompatActivity {
+public class Addpayment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editfeedback);
+        setContentView(R.layout.activity_addpayment);
     }
 }
