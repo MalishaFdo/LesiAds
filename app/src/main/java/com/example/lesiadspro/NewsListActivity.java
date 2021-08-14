90698a4362fcaed94bb6e104e553666e35be8e0a
Maleesha Fernando <maleeshafernando31@gmail.com>
2021-08-14 15:48:12 +0530

    Intigrate Newspaper List

---
+++ app/src/main/java/com/example/lesiadspro/NewsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.lesiadspro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
