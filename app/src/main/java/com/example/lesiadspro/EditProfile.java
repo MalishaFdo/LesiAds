9c76d3cb4b89d4078a08865aea8da15c4afb5609
vibhujay99 <87332777+vibhujay99@users.noreply.github.com>
2021-08-15 01:57:56 +0530

    Navigation Drawer

--- app/src/main/java/com/example/lesiadspro/EditProfile.java
+++ app/src/main/java/com/example/lesiadspro/EditProfile.java
@@ -2,13 +2,29 @@ package com.example.lesiadspro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class EditProfile extends AppCompatActivity {
+
+    TextView photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        photo = (TextView)findViewById(R.id.textView10);
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent12 = new Intent(EditProfile.this,AddPhoto.class);
+                startActivity(intent12);
+            }
+        });
+
+
     }
 }
