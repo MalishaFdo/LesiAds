af9ebdfb5cb8c47a32370fea840a024a3f06ec84
Bihini Amarasinghe <bihiniamarasinghe98@gmail.com>
2021-08-14 18:54:08 +0530

    Checkout Page

---
+++ app/src/main/java/com/example/lesiadspro/payCheckout.java
@@ -0,0 +1,14 @@
+package com.example.lesiadspro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class payCheckout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_checkout);
+    }
+}
