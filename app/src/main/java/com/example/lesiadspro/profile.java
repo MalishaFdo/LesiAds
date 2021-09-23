22d55050ce8ef3d8ce8d3c6dfff900f2f227d178
vibhujay99 <87332777+vibhujay99@users.noreply.github.com>
2021-09-24 01:47:39 +0530

    Fetch data from cloud firestore

--- app/src/main/java/com/example/lesiadspro/profile.java
+++ app/src/main/java/com/example/lesiadspro/profile.java
@@ -1,5 +1,6 @@
 package com.example.lesiadspro;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,11 +11,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class profile extends AppCompatActivity {
 
+    TextView firstname,lastname,email,username;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+    String userID;
 
 
     Button editprofile;
@@ -25,6 +36,28 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        firstname = findViewById(R.id.firstName);
+        lastname = findViewById(R.id.lastName);
+        email = findViewById(R.id.Email);
+        username = findViewById(R.id.userName);
+
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fstore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                firstname.setText(value.getString("fname"));
+                lastname.setText(value.getString("lname"));
+                email.setText(value.getString("email"));
+                username.setText(value.getString("username"));
+
+            }
+        });
 
 
 
