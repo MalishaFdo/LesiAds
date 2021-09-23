528d796f796593fc2e600a8ef99398ccb90da51b
vibhujay99 <87332777+vibhujay99@users.noreply.github.com>
2021-09-24 01:16:24 +0530

    Data insert to cloud firestore

--- app/src/main/java/com/example/lesiadspro/RegisterActivity.java
+++ app/src/main/java/com/example/lesiadspro/RegisterActivity.java
@@ -1,11 +1,14 @@
 package com.example.lesiadspro;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,14 +17,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText mFirstName,mLastName,mUserName, mEmail,mPassword,mPhone;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+    String userID;
 
     TextView signin;
 
@@ -40,6 +52,7 @@ public class RegisterActivity extends AppCompatActivity {
         mEmail = findViewById(R.id.Email);
         mPassword = findViewById(R.id.password);
         mPhone = findViewById(R.id.phone);
+        fstore = FirebaseFirestore.getInstance();
 
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
@@ -64,6 +77,10 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String firstname = mFirstName.getText().toString();
+                String lastname = mLastName.getText().toString();
+                String phone = mPhone.getText().toString();
+                String username = mUserName.getText().toString();
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required . ");
@@ -88,6 +105,26 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             Toast.makeText(RegisterActivity.this,"User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fstore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fname", firstname);
+                            user.put("lname", lastname);
+                            user.put("email", email);
+                            user.put("phone", phone);
+                            user.put("username", username);
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG,"onSuccess: user profile is created for "+ userID );
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: "+ e.toString());
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(),profile.class));
 
                         }else {
