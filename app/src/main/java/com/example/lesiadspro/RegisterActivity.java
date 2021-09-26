dfe213498313092c4b1bfa4a97a261a72a6d8b4a
vibhujay99 <87332777+vibhujay99@users.noreply.github.com>
2021-09-26 13:12:19 +0530

    Update and Delete action buttons 2

--- app/src/main/java/com/example/lesiadspro/RegisterActivity.java
+++ app/src/main/java/com/example/lesiadspro/RegisterActivity.java
@@ -38,11 +38,8 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseDatabase rootnode;
     DatabaseReference reference;
     String userID;
-
     TextView signin;
-
     Button signup;
-
     TextView addphoto;
 
     @Override
@@ -57,7 +54,6 @@ public class RegisterActivity extends AppCompatActivity {
         mPassword = findViewById(R.id.password);
         mPhone = findViewById(R.id.phone);
         fstore = FirebaseFirestore.getInstance();
-
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
@@ -84,7 +80,6 @@ public class RegisterActivity extends AppCompatActivity {
                 reference = rootnode.getReference("users");
 
                 //get values
-
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
                 String firstname = mFirstName.getText().toString();
