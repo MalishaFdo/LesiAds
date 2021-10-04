875ae1eab9b74dee5266d05d3c7d9700661eff1f
Bihini Amarasinghe <bihiniamarasinghe98@gmail.com>
2021-10-05 02:27:54 +0530

    Read Payment Details

--- app/src/main/java/com/example/lesiadspro/Addpayment.java
+++ app/src/main/java/com/example/lesiadspro/Addpayment.java
@@ -119,6 +119,7 @@ public class Addpayment extends AppCompatActivity {
                 Intent intent = new Intent(Addpayment.this, viewPaymentMain.class);
                 startActivity(intent);
 //o
+                //pp
             }
         } catch (NumberFormatException e) {
             Toast.makeText(getApplicationContext(), "Invalid Card Number !!!", Toast.LENGTH_SHORT).show();
